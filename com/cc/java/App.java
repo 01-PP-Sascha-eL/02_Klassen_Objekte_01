4caafa473e3b80908364fe005fd60896a591eb78
ShatterAr <sascha.s.89@gmx.de>
2023-05-22 11:15:36 +0200

    Milestone 1

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -2,7 +2,23 @@ package com.cc.java;
 
 public class App {
     
+    public static void main(String[] args) {
     
+        /* Instanziierung Klasse --> Objekt */
+        // Cat -- Referenztyp
+        // cat -- Referenzvariable
 
+        Cat cat1 = new Cat(); 
+        System.out.println("Blick von aussen: " + cat1);
+        cat1.tellYourAdress();
+
+        System.out.println( "----------------");
+
+        Cat cat2 = new Cat(); 
+        System.out.println("Blick von aussen: " + cat2);
+        cat2.tellYourAdress();
+
+
+    }
 }
 
---
+++ com/cc/java/Cat.java
@@ -0,0 +1,10 @@
+package com.cc.java;
+
+public class Cat {
+
+    // this --> Instanzvariable
+    public void tellYourAdress() {
+        System.out.println("Blick von innen: " + this);
+        
+    }
+}
