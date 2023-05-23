cb1143c2642e4d2fab2e276cb0fdc8544a1c6c4a
ShatterAr <sascha.s.89@gmx.de>
2023-05-23 12:20:37 +0200

    mod3

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -2,6 +2,34 @@ package com.cc.java;
 
 public class Cat {
 
+    private String name;
+    private String furColor; 
+    private int age;
+
+
+
+    public Cat(String name, String furColor, int age) {
+        this.name = name;
+        this.furColor = furColor;
+        this.age = age;
+    }
+
+    public String tellYourName() {
+        return this.name;
+
+    }
+
+    public String tellYourFurColor() {
+        return this.furColor;
+
+    }
+
+    public int tellYourAge() {
+        return this.age;
+
+    }
+
+
     // this --> Instanzvariable // Selbstreferenz
     public String tellYourAdress() {
         return "Blick von innen: " + this;
